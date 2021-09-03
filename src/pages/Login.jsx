a62e3b1ceee47c65d8bed1c6196e8cb32104f0bb
suco <francalucas937@gmail.com>
2021-09-03 13:46:56 -0300

    add requisito 1 e 2 refatorados

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Redirect } from 'react-router';
-import fetchTokenApi from './pageFunctions/loginFuncs';
 import { Link } from 'react-router-dom';
+import fetchTokenApi from './pageFunctions/loginFuncs';
 import logo from '../trivia.png';
 import Input from '../components/Input';
 import Button from '../components/Button';
